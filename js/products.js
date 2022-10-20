1c3f68dc255f47d244be99f8223c3ed6bfceadee
corrado1982 <104769882+corrado1982@users.noreply.github.com>
2022-10-20 15:48:44 +0200

    first finished

--- js/products.js
+++ js/products.js
@@ -14,3 +14,28 @@ const products = [
         soldOut: true,
     },
 ];
+
+let selectContainer = document.querySelector(".container");
+
+    let items = "";
+for (let i = 0; i < products.length; i++) {
+    
+    //console.log(items)
+
+let price = "Contact us"
+if (isNaN(products[i].price)) {
+        products[i].price = price ;
+}
+let nameColor = "green";
+
+if (products[i].soldOut) {
+    nameColor = "red" ; 
+}
+
+items +=`   <div><p style="color: ${nameColor}">  ${products[i].name} </p>
+            <p> Price: ${products[i].price} </p>
+            </div>
+`;}
+
+selectContainer.innerHTML = items;
+
