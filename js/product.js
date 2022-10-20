966209ce1a36e76daf2df1fe10cccb2d25dda881
corrado1982 <104769882+corrado1982@users.noreply.github.com>
2022-10-20 16:17:27 +0200

    w

--- js/product.js
+++ js/product.js
@@ -3,3 +3,14 @@ const product = {
     price: 99,
     onSale: true,
 };
+
+const displayProperty = document.querySelector(".container");
+
+let textUnderlined = "underline";
+
+
+displayProperty.innerHTML = `<div>
+<h4 style="text-decoration:${textUnderlined }">${product.name}</h4>
+<p>${product.price}</p>
+</div> `;
+
--- js/products.js
+++ js/products.js
@@ -20,22 +20,23 @@ let selectContainer = document.querySelector(".container");
     let items = "";
 for (let i = 0; i < products.length; i++) {
     
-    //console.log(items)
 
-let price = "Contact us"
+let price = "Contact us";
+
 if (isNaN(products[i].price)) {
         products[i].price = price ;
 }
+
 let nameColor = "green";
 
 if (products[i].soldOut) {
     nameColor = "red" ; 
 }
 
-items +=`   <div><p style="color: ${nameColor}">  ${products[i].name} </p>
-            <p> Price: ${products[i].price} </p>
-            </div>
-`;}
+items +=`<div><p style="color: ${nameColor}"> ${products[i].name} </p>
+                <p> Price: ${products[i].price} </p>
+        </div>`;
+}
 
 selectContainer.innerHTML = items;
 
